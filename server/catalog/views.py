fdc1ab4ba79d0d96f3494859c55d9938bf7b0b6a
Kostis Bourlas <kostisbourlas@protonmail.com>
2021-08-09 13:11:47 +0300

    Added more tests

--- server/catalog/views.py
+++ server/catalog/views.py
@@ -1,5 +1,6 @@
 
 from rest_framework.generics import ListAPIView
+from django.db.models import Q
 
 from .models import Wine
 from .serializers import WineSerializer
@@ -7,6 +8,26 @@ from .filters import WineFilterSet
 
 
 class WinesView(ListAPIView):
-    queryset = Wine.objects.all()
+    # queryset = Wine.objects.all()
     serializer_class = WineSerializer
-    filterset_class = WineFilterSet
+    # filterset_class = WineFilterSet
+
+    def get_queryset(self):
+        queryset = Wine.objects.all()
+        country = self.request.query_params.get('country')
+        if country is not None:
+            queryset = queryset.filter(country=country)
+
+        points = self.request.query_params.get('points')
+        if points:
+            queryset = queryset.filter(points=points)
+        
+        query = self.request.query_params.get('query')
+        if query:
+            queryset = queryset.filter(Q(
+                Q(variety__contains=query) |
+                Q(winery__contains=query) |
+                Q(description__contains=query)
+            ))
+        
+        return queryset
